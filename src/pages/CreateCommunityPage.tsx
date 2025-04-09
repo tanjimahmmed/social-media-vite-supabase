918ba22a86b7848815c1bc2558dc7c3bd2c67882
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-04-09 22:17:11 +0600

    first commit

---
+++ src/pages/CreateCommunityPage.tsx
@@ -0,0 +1,7 @@
+import { CreateCommunity } from "../components/CreateCommunity"
+
+export const CreateCommunityPage = () => {
+    return (<div className="pt-20">
+        <CreateCommunity />
+    </div>)
+}
---
+++ src/pages/CreatePostPage.tsx
@@ -0,0 +1,8 @@
+import { CreatePost } from "../components/CreatePost"
+
+export const CreatePostPage = () => {
+    return (<div className="pt-20">
+        <h2 className="text-6xl font-bold mb-6 text-center bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent">Create new post</h2>
+        <CreatePost/>
+    </div>)
+}
